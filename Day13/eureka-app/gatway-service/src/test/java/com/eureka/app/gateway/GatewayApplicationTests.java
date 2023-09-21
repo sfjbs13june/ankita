6dd4e3a6fa2758665aa81a870ada291fc77744c7
EC31DR <ankita.chaurasia@ing.com>
2023-09-21 09:50:46 +0530

    day 13

---
+++ Day13/eureka-app/gatway-service/src/test/java/com/eureka/app/gateway/GatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package Day13.eureka/*package com.eureka.app.gateway;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayApplicationTests {
+
+}*/
---
+++ Day13/eureka-app/school-service/src/main/java/com/eureka/app/school/SchoolApplication.java
@@ -0,0 +1,13 @@
+package Day13.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SchoolApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+	}
+}
