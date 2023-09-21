9f1ec6e1865a1919164b09a2321380532b40e8ca
EC31DR <ankita.chaurasia@ing.com>
2023-09-21 10:24:38 +0530

    Day 14 spring-header

---
+++ Day14/spring-header-app/src/main/java/com/ankita/app/SpringHeaderAppApplication.java
@@ -0,0 +1,13 @@
+package Day14.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHeaderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHeaderAppApplication.class, args);
+	}
+
+}
